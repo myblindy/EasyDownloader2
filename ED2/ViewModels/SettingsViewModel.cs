dd0a815e7c7c71bbea347687d68f2e3a6682529a
Marius <tweety.04.01@gmail.com>
2022-12-31 18:32:19 -0500

    slight about improvements!

--- ED2/ViewModels/SettingsViewModel.cs
+++ ED2/ViewModels/SettingsViewModel.cs
@@ -59,18 +59,14 @@ public partial class SettingsViewModel : ObservableRecipient
     private static string GetVersionDescription()
     {
         Version version;
-
         if (RuntimeHelper.IsMSIX)
         {
             var packageVersion = Package.Current.Id.Version;
-
             version = new(packageVersion.Major, packageVersion.Minor, packageVersion.Build, packageVersion.Revision);
         }
         else
-        {
             version = Assembly.GetExecutingAssembly().GetName().Version!;
-        }
 
-        return $"{"AppDisplayName".GetLocalized()} - {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
+        return $"App version {version.Major}.{version.Minor}.{version.Build}.{version.Revision}";
     }
 }
